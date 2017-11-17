0b6bd6a8274611754eeb7e4c009a588ed475dd21
rishavs <rishav.sharan@live.com>
2017-11-17 11:20:18 +0530

    broken form

--- src/components/postEdit/index.js
+++ src/components/postEdit/index.js
@@ -2,27 +2,40 @@ import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
 
+import Backend from '../../lib/Backend';
+
 
 // various props to use here will be type (new, edit, read) and post (empty, {})
 
 export default class PostForm extends Component {
+   
+    constructor(props) {
+        super(props);
+        this.state = { 
+            postId: props.postData.id,
+            postTitle: props.postData.title,
+            postContent: props.postData.content
+        };
+    }
 
-    render( props, postData) {
+    handleSubmit = (event) => {
+        alert(this.state.postTitle);
+    }
+
+    render( ) {
         return (
             <div class={style.home}>
                 <h1> EDIT Post Details: </h1>
-                
-                <label>
-                    Title:
-                    <input type="text" value={props.postData.title} />
-                </label>
+               
+                <form onSubmit={this.handleSubmit} action="javascript:">
+                    <label>Title:</label>
+                    <input type="text" value={this.state.postTitle} onInput={linkState(this, 'postTitle')} />
 
-                <label>
-                    Content:
-                    <textarea value={props.postData.content} />
-                </label>
-
-                <button type="button" onClick={ props.onTogl }> Save Post </button>
+                    <label> Content:</label>
+                    <textarea value={this.state.postContent} onInput={linkState(this, 'postContent')} />
+                    
+                    <button type="submit"> Save Post </button>
+                </form>
 
             </div>
         );
--- src/lib/Backend.js
+++ src/lib/Backend.js
@@ -14,6 +14,27 @@ var Backend = {
 	    const json = await response.json();
 	    return json
 	},
+
+	save_current_post: async function(item){
+	    const response = await fetch("https://data.diesel16.hasura-app.io/v1/template/save_post", {
+		    method: 'post',
+		    body: JSON.stringify({	
+		    	"id": item.id,
+		    	"title": item.title,
+		    	"content": item.content,
+		    })
+	  	})
+	    const json = await response.json();
+	    return json
+	},
+	delete_current_post: async function(id){
+	    const response = await fetch("https://data.diesel16.hasura-app.io/v1/template/delete_post", {
+		    method: 'post',
+		    body: JSON.stringify({"id": id})
+	  	})
+	    const json = await response.json();
+	    return json
+	},
 }
 
 export default Backend
